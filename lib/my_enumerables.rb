1c88516383f2ab9609fd966c907bc7ddc492f273
Kropyls <Kropyls@gmail.com>
2022-07-16 21:54:59 -0700

    re-created some basic enum methods. Really helped clear up my understanding of what is happening behind the scenes. And got a change to use lambdas too!

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,51 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    count = 0
+    my_each do |el|
+      yield el, count
+      count += 1
+    end
+  end
+
+  def my_select
+    result = []
+    my_each { |el| result.push(el) if yield el }
+    result
+  end
+
+  def my_all?
+    my_each { |el| return false unless yield el }
+    true
+  end
+
+  def my_any?
+    my_each { |el| return true if yield el }
+    false
+  end
+
+  def my_none?
+    my_each { |el| return false if yield el }
+    true
+  end
+
+  def my_count
+    count = 0
+    block = block_given? ? ->(el) { count += 1 if yield el } : ->(_el) { count += 1 }
+    my_each(&block)
+    count
+  end
+
+  def my_map
+    result = []
+    my_each { |el| result.push(yield el) }
+    result
+  end
+
+  def my_inject(acc = 0)
+    my_each { |el| acc = yield acc, el }
+    acc
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +54,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for el in self
+      yield el
+    end
+  end
 end
